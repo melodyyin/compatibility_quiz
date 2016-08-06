7c6f99623bfc032a86f7fc92b4f77bd705db58b1
melody <melodyyin18@gmail.com>
2016-08-06 12:42:07 -0700

    test change

--- tests/test-compatibility-quiz.R
+++ tests/test-compatibility-quiz.R
@@ -5,11 +5,15 @@ test_that("scores do not contain NAs", {
 })
 
 test_that("to_binary_vector is a binary vector", {
-  rand_qa = sample_n(mc_choices, 1) 
+  test_qa = list(question="q5", choice="choice1")  
+  test_possibs = data.frame(question=rep("q5", 5),
+                            choice=c("choice1", "choice2", "choice3", "choice4", "choice5"))
   
-  expect_false(anyNA(to_binary_vector(rand_qa$question, rand_qa$choice)))
-  expect_is(to_binary_vector(rand_qa$question, rand_qa$choice), "numeric")
-  expect_true(all(c(0,1) %in% to_binary_vector(rand_qa$question, rand_qa$choice)))
+  expect_false(anyNA(to_binary_vector(test_qa$question, test_qa$choice, test_possibs))) 
+  expect_is(to_binary_vector(test_qa$question, test_qa$choice, test_possibs), "numeric")
+  expect_equal(to_binary_vector(test_qa$question, test_qa$choice, test_possibs), c(1,0,0,0,0))
+  expect_equal(to_binary_vector(test_qa$question, "choice2", test_possibs), c(0,1,0,0,0))
+  expect_error(to_binary_vector(test_qa$question, "choice10", test_possibs))
 })
 
 test_that("to_sentiment_score returns the correct word count", {
@@ -19,10 +23,8 @@ test_that("to_sentiment_score returns the correct word count", {
 })
 
 test_that("get_matrix has colSum of 1", {
-  rand_q = sample(unique(raw$question), 1)
-  
-  expect_is(get_matrix(rand_q), "matrix")
-  expect_equal(unique(colSums(get_matrix(rand_q))), 1)
+  expect_is(get_matrix("q5"), "matrix")
+  expect_equal(unique(colSums(get_matrix("q5"))), 1)
 })
 
 test_that("everyone has matches", {
